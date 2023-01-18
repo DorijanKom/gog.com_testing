5c71768f31c033d5c4d9257327bd6903b327b3d5
DorijanKom <dorijan.komsic@stu.ibu.edu.ba>
2023-01-18 23:01:31 +0100

    Added test files for part 1 and 2

---
+++ src/test/java/org/example/part1/Test1.java
@@ -0,0 +1,16 @@
+package org.example.part1;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+public class Test1 {
+}
